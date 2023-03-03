02dfa9ea75a7dee9d688d347d045fa1b0d701caa
Khashyrova <121459042+Khashyrova@users.noreply.github.com>
2023-03-03 12:31:16 +0000

    Deploying to gh-pages from @ Khashyrova/goit-js-hw-09@c7814040a3ac7518445b53f520fc8796b0af19b4 🚀

--- 01-color-switcher.f3f257de.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){o=setInterval(r,1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(o),e.disabled=!0}));let o=null;function r(){n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}
-//# sourceMappingURL=01-color-switcher.f3f257de.js.map
